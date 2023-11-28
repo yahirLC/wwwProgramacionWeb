c860cfe2c1494acffafb8283cc0c8830dc2eb1d5
Yahir Lagunes <yahirlagunesceballos@gmail.com>
2023-11-28 17:44:09 -0600

    actualizacion profe 2811

--- core/app/action/users-action.php
+++ core/app/action/users-action.php
@@ -5,6 +5,8 @@ if(!isset($_SESSION['user_id'])) Core::redir("./");
 
 if(isset($_GET["opt"]) && $_GET["opt"] == "add"){
 
+
+
 	if(isset($_POST["nombre"]) and isset($_POST["email"]) and isset($_POST["username"])
 	 and isset($_POST["password"])){
 
@@ -19,21 +21,54 @@ if(isset($_GET["opt"]) && $_GET["opt"] == "add"){
 
 		$u->add();
 
+		Core::addToastr('success','Usuario agregado con Exito .!');
+		Core::redir("./?view=users&opt=all");
+
 	}else{
 
 		echo "NO";
 	}
 
 
+}else if(isset($_GET["opt"]) && $_GET["opt"] == "update" ){
+
+
+		$u = UserData::getByID($_POST['user_id']);
+
+		$u->nombre = $_POST['nombre'];
+		$u->apellido = $_POST['apellido'];
+		$u->email = $_POST['email'];
+		$u->username = $_POST['username'];
+		//$u->password = sha1(md5($_POST['password']));
+		//$u->kind = 1;
+		//$u->status = 1;
+
+		$u->update();
+
+		if(isset($_POST['password']) and $_POST['password']!=''  ){
+
+			$u->password = sha1(md5($_POST['password']));
+
+			$u->updatePass();
+		}
+
+		Core::addToastr('success','Usuario actualizado con Exito .!');
+		Core::redir("./?view=users&opt=all");
+
+
+}else if(isset($_GET["opt"]) && $_GET["opt"] == "delete" && 
+	isset($_GET["id"]) ){
+
+	$user = UserData::getByID($_GET['id']);
+
+	$user->updateOne("status",0);
+
+	Core::addToastr('warning','Usuario eliminado con Exito .!');
+	Core::redir("./?view=users&opt=all");
+
+
 }
 
-if (isset($_GET["opt"]) && $_GET["opt"] == "delete") {
-    if (isset($_POST["usuariosEliminar"]) && is_array($_POST["usuariosEliminar"])) {
-        foreach ($_POST["usuariosEliminar"] as $userId) {
-            UserData::deleteById($userId);
 
 
-        }
-    }
-}
 ?>
--- core/app/view/users-view.php
+++ core/app/view/users-view.php
@@ -23,21 +23,22 @@ if(isset($_GET["opt"]) && $_GET["opt"] == "all"){
 			<a class="btn btn-primary pull-right" href="./?view=users&opt=add"><i class="bi-persons"> </i>
 			 Nuevo</a>
 
-			 <a class="btn btn-danger pull-right mx-2" href="./?view=users&opt=delete"><i class="bi-persons"> </i>
-                    Eliminar</a>
-
 		</div>
+		<div class="card">
+        <div class="card-body">
 		<?php
 			if(count($listaUsuarios)>0){
 		?>
+		
 			<div class="bd-example table-responsive">
-		        <table class="table table-striped">
+		        <table class="table table-striped table-hover ">
 		            <thead>
 		            <tr>
 		                <th scope="col">#</th>
 		                <th scope="col">Nombre</th>
 		                <th scope="col">Username</th>
 		                <th scope="col">Email</th>
+		                <th></th>
 		            </tr>
 		            </thead>
 		            <tbody>
@@ -46,20 +47,26 @@ if(isset($_GET["opt"]) && $_GET["opt"] == "all"){
 		            ?>
 		            		<tr>
 		            			<td><?php echo $row->id; ?></td>
-		            			<td><?php echo $row->nombre; ?></td>
+		            			<td><?php echo $row->nombre.' '.$row->apellido; ?></td>
 		            			<td><?php echo $row->username; ?></td>
 		            			<td><?php echo $row->email; ?></td>
+		            			<td><a href="./?view=users&opt=edit&id=<?php echo $row->id;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Editar </a> </td>
+
+		            			<td>
+		            				<button 
+		            					data-id="<?php echo $row->id;?>" 
+		            					data-name="<?php echo $row->nombre;?>"  
+		            					class="deleteRow btn btn-danger"><i class="fa fa-pencil"></i> Elimiar </button> 
+		            			</td>
 		            		</tr>
 
 		            <?php
 		            	}
-
-		            ?>	
-		            
+		            ?>			            
 		            </tbody>
 		        </table>
 		    </div>
-    
+    	
 			<?php
 
 		}else{
@@ -77,148 +84,169 @@ if(isset($_GET["opt"]) && $_GET["opt"] == "all"){
 		}
 		?>
 
-
+			</div>
+		</div>
 
 	</div>
 
 
 </div>
 
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".deleteRow").click(function(){
+
+			var id = $(this).attr('data-id');
+			var name = $(this).attr('data-name');
+
+			r = confirm("¿Seguro que deseas eliminar el registro "+name+"? ");
+
+			if(r==true){
+				window.location= "./?action=users&opt=delete&id="+id;	
+			}
+
+		});
+
+
+	});
+
+</script>
+
 <?php
 
-}elseif(isset($_GET["opt"]) && $_GET["opt"] == "add"){
+}else if(isset($_GET["opt"]) && $_GET["opt"] == "add"){
 ?>
 <div class="content-inner container-fluid pb-0" id="page_layout">
-<div class="col-xl-12 col-lg-12">
-   <div class="card">
-      <div class="card-header d-flex justify-content-between">
-         <div class="header-title">
-            <h4 class="card-title">Nuevo usuario </h4>
-         </div>
-      </div>
-      <div class="card-body">
-         <div class="new-user-info">
-            <form method="post" action="./?action=users&opt=add">
-               <div class="row">
-                  <div class="form-group col-md-6">
-                     <label class="form-label" for="fname">Nombre:</label>
-                     <input type="text" class="form-control" name="nombre" id="fname" placeholder="Nombre">
-                  </div>
-                  <div class="form-group col-md-6">
-                     <label class="form-label" for="lname">Last Name:</label>
-                     <input type="text" class="form-control" id="lname"  name="apellido" placeholder="Last Name">
-                  </div>
-                  
-                  <div class="form-group col-md-12">
-                     <label class="form-label" for="email">Email:</label>
-                     <input type="email" class="form-control" name="email" id="email" placeholder="Email">
-                  </div>
-                  
-               </div>
-               <hr>
-               <h5 class="mb-3">Security</h5>
-               <div class="row">
-                  <div class="form-group col-md-12">
-                     <label class="form-label" for="uname">User Name:</label>
-                     <input type="text" class="form-control" id="uname" name="username" placeholder="User Name">
-                  </div>
-                  <div class="form-group col-md-6">
-                     <label class="form-label" for="pass">Password:</label>
-                     <input type="password" class="form-control" id="pass" name="password" placeholder="Password">
-                  </div>
-                  <div class="form-group col-md-6">
-                     <label class="form-label" for="rpass">Repeat Password:</label>
-                     <input type="password" class="form-control" id="rpass" placeholder="Repeat Password ">
-                  </div>
-               </div>
-               
-               <button type="submit" class="btn btn-primary">Agregar usuario</button>
-            </form>
-         </div>
-      </div>
-   </div>
-</div>
+	<div class="col-xl-12 col-lg-12">
+	   <div class="card">
+	      <div class="card-header d-flex justify-content-between">
+	         <div class="header-title">
+	            <h4 class="card-title">Nuevo usuario </h4>
+	         </div>
+	      </div>
+	      <div class="card-body">
+	         <div class="new-user-info">
+	            <form method="post" action="./?action=users&opt=add">
+	               <div class="row">
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="fname">Nombre:</label>
+	                     <input type="text" class="form-control" name="nombre" id="fname" placeholder="Nombre">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="lname">Last Name:</label>
+	                     <input type="text" class="form-control" id="lname"  name="apellido" placeholder="Last Name">
+	                  </div>
+	                  
+	                  <div class="form-group col-md-12">
+	                     <label class="form-label" for="email">Email:</label>
+	                     <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+	                  </div>
+	                  
+	               </div>
+	               <hr>
+	               <h5 class="mb-3">Security</h5>
+	               <div class="row">
+	                  <div class="form-group col-md-12">
+	                     <label class="form-label" for="uname">User Name:</label>
+	                     <input type="text" class="form-control" id="uname" name="username" placeholder="User Name">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="pass">Password:</label>
+	                     <input type="password" class="form-control" id="pass" name="password" placeholder="Password">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="rpass">Repeat Password:</label>
+	                     <input type="password" class="form-control" id="rpass" placeholder="Repeat Password ">
+	                  </div>
+	               </div>
+	               
+	               <button type="submit" class="btn btn-primary">Agregar usuario</button>
+	            </form>
+	         </div>
+	      </div>
+	   </div>
+	</div>
 </div>
 
 <?php
 
-}elseif(isset($_GET["opt"]) && $_GET["opt"] == "delete"){
+}else if(isset($_GET["opt"]) && $_GET["opt"] == "edit"){
+		//Todas las validaciones
+		if(!isset($_GET["id"]) or $_GET["id"] == "")
+		{
+			Core::addToastr('info',"Ups un error");			
+			Core::redir("./?view=users&opt=all");
+
+		}
+
+
+			//validar si es numero
+			$user = UserData::getByID($_GET['id']);
+			$found = false;
+			if($user!=null){
+					$found=true;
+			}
+
+		
+if($found){
 ?>
 
 
-    <!-- Agrega esta sección para mostrar la lista de usuarios -->
-    <div class="col-xl-12 col-lg-12">
-        <div class="card">
-            <div class="card-header d-flex justify-content-between">
-                <div class="header-title">
-                    <h4 class="card-title">Eliminar usuarios</h4>
-                </div>
-            </div>
-            <div class="card-body">
-                <form id="deleteForm" method="post" action="./?action=users&opt=delete" onsubmit="return confirmDelete();">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Nombre</th>
-                                <th>Username</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            // Reemplaza esta parte con tu lógica para obtener la lista de usuarios desde la base de datos
-                            $listaUsuarios = UserData::getUsersbyKind();
+<div class="content-inner container-fluid pb-0" id="page_layout">
+	<div class="col-xl-12 col-lg-12">
+	   <div class="card">
+	      <div class="card-header d-flex justify-content-between">
+	         <div class="header-title">
+	            <h4 class="card-title">Editar usuario, <b><?php echo $user->nombre;?></b> </h4>
+	         </div>
+	      </div>
+	      <div class="card-body">
+	         <div class="new-user-info">
+	            <form method="post" action="./?action=users&opt=update">
+	            	<input type="hidden" name="user_id" value="<?php echo $user->id;?>">
+	               <div class="row">
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="fname">Nombre:</label>
+	                     <input type="text" class="form-control" value="<?php echo $user->nombre; ?>" name="nombre" id="fname" placeholder="Nombre">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="lname">Last Name:</label>
+	                     <input type="text" class="form-control" id="lname" value="<?php echo $user->apellido; ?>"  name="apellido" placeholder="Last Name">
+	                  </div>
+	                  
+	                  <div class="form-group col-md-12">
+	                     <label class="form-label" for="email">Email:</label>
+	                     <input  type="email" value="<?php echo $user->email; ?>" class="form-control" name="email" id="email" placeholder="Email">
+	                  </div>
+	                  
+	               </div>
+	               <hr>
+	               <h5 class="mb-3">Security</h5>
+	               <div class="row">
+	                  <div class="form-group col-md-12">
+	                     <label class="form-label" for="uname">User Name:</label>
+	                     <input type="text" value="<?php echo $user->username; ?>" class="form-control" id="uname" name="username" placeholder="User Name">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="pass">Password:</label>
+	                     <input type="password" class="form-control" id="pass" name="password" placeholder="Password">
+	                  </div>
+	                  <div class="form-group col-md-6">
+	                     <label class="form-label" for="rpass">Repeat Password:</label>
+	                     <input type="password" class="form-control" id="rpass" placeholder="Repeat Password ">
+	                  </div>
+	               </div>
+	               
+	               <button type="submit" class="btn btn-success">Actualizar usuario</button>
+	            </form>
+	         </div>
+	      </div>
+	   </div>
+	</div>
+</div>
+<?php
+} // end if found
 
-                            if (count($listaUsuarios) > 0) {
-                                foreach ($listaUsuarios as $user) {
-                                    echo "<tr>";
-                                    echo "<td><input type='checkbox' name='usuariosEliminar[]' value='{$user->id}'></td>";
-                                    echo "<td>{$user->nombre}</td>";
-                                    echo "<td>{$user->username}</td>";
-                                    echo "<td>{$user->email}</td>";
-                                    echo "</tr>";
-                                }
-                            } else {
-                                echo "<tr><td colspan='4'>No hay usuarios para mostrar</td></tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                    <?php
-                    if (count($listaUsuarios) > 0) {
-                    ?>
-                        <button type="submit" class="btn btn-danger">Eliminar usuarios seleccionados</button>
-                    <?php
-                    }
-                    ?>
-                </form>
-            </div>
-        </div>
-    </div>
 
-    <script>
-        function confirmDelete() {
-            var checkboxes = document.getElementsByName('usuariosEliminar[]');
-            var selectedUsers = [];
-
-            for (var i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].checked) {
-                    selectedUsers.push(checkboxes[i].value);
-                }
-            }
-
-            if (selectedUsers.length > 0) {
-                var confirmation = confirm("¿Seguro que quieres eliminar al usuario con ID=" + selectedUsers.join(', ') + "?");
-
-                return confirmation;
-            } else {
-                alert("Por favor, selecciona al menos un usuario para eliminar.");
-                return false;
-            }
-        }
-    </script>
-
-    <?php
 }
 ?>
